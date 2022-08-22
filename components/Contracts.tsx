fbef3eb14ba60ec9e6437eee6d809ba9a82ecd01
Sam Ingle <sam@untoldhq.com>
2022-08-22 15:12:59 -0700

    basic top level tabs

---
+++ components/Contracts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contracts: React.FC = () => {
+  
+  return (
+    <div className="Contracts">
+      Contracts
+    </div>
+  )
+}
+
+export default Contracts
---
+++ components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+  
+  return (
+    <div className="Dashboard">
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
