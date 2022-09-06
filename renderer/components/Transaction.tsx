b34341a288d88b93fbad44335698bcd7c3369c1f
Nat McConnaughay <nat@untoldhq.com>
2022-09-06 12:56:15 -0500

    dashboard feedback

--- renderer/components/Transaction.tsx
+++ renderer/components/Transaction.tsx
@@ -2,8 +2,10 @@ import React from "react"
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import json from 'react-syntax-highlighter/dist/cjs/languages/hljs/json'
 import docco from 'react-syntax-highlighter/dist/cjs/styles/hljs/docco'
+import bash from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash'
 
 SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('bash', bash);
 
 import { useAppSelector } from '../store/'
 import { transactionSelectors } from '../store/transactions'
@@ -23,16 +25,25 @@ const Transaction: React.FC<TransactionProps> = ({ transactionHash }) => {
     }
   })
 
+  const snippet = "wasmd tx bank send \\\n$(wasmd keys show [key1] -a)  $(wasmd keys show [key2] -a) \\\n1stake --chain-id [chain-23] -y --output json"
+
   return (
-    <div className="p-6">
+    <div className="p-6 h-full">
       { transaction === undefined &&
-        <span className="text-2xl flex font-semibold text-gray-900">
-          Select a transaction from the list to the left to inspect it
-        </span>
+        <div className="flex flex-col justify-center items-center h-full w-full space-y-4 text-center">
+          <p>
+            Select a transaction from the list to the left to inspect it. <br />If there are no transactions, send a transation using the following command, inserting your own key names and chain id:
+          </p>
+          <div className="text-left mt-4">
+            <SyntaxHighlighter language="bash" style={docco} customStyle={{borderRadius: "0.25rem", background: "#D3FDF9"}}>
+              {snippet}
+            </SyntaxHighlighter>
+          </div>
+        </div>
       }
       { transactionHash !== undefined &&
         <React.Fragment>
-          <span className="text-2xl flex font-semibold text-gray-900">Transaction</span>
+          <span className="text-2xl flex font-semibold text-gray-900 mx-4">Transaction</span>
           <SyntaxHighlighter language="json" style={docco} customStyle={{borderRadius: "0.25rem", background: "#D3FDF9", margin: "1rem"}}>
             {JSON.stringify(transaction, null, 2)}
           </SyntaxHighlighter>
